05d5ae91db0bc77413721bb97b98a60cff73e494
jyennes <jyennes5@gmail.com>
2020-11-03 22:39:40 -0500

    Basic user roles and privilege's
    # updated index.js to give results based on roles.

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,34 @@
 module.exports = {
     getHomePage: (req, res) => {
-         let email = req.user.email 
-         let query = "SELECT * FROM `patient` WHERE email = '" + email + "' ORDER BY id ASC";
+         let email = req.user.email;
+         let role = req.user.role; 
+         let roleQuery = "SELECT * FROM `users` ORDER BY id ASC";
+         let pQuery = "SELECT * FROM `patient` WHERE email = '" + email + "' ORDER BY id ASC";
+         let dQuery = "SELECT * FROM `patient` ORDER BY id ASC";
 
-        con.query(query, (err, result) => {
-            if (err) {
-                res.redirect('/');
+        con.query(roleQuery, (err, result) => {
+            if (role == `doctor`) {
+                con.query(dQuery, (err, result) => {
+                    if (err) {
+                        res.redirect('/');
+                    }
+                    res.render('index.ejs', {
+                        title: "EMR Title"
+                        ,patient: result
+                    });
+                });
             }
-            res.render('index.ejs', {
-                title: "EMR Title"
-                ,patient: result
-            });
-        });
+            else {
+                con.query(pQuery, (err, result) => {
+                    if (err) {
+                        res.redirect('/');
+                    }
+                    res.render('index.ejs', {
+                        title: "EMR Title"
+                        ,patient: result
+                    });
+                });
+            }
+        })
     },
 };
